95a774e2ab4f35cefccf20e979c8cd360e0b0772
Seth Puckett <puckett.seth@gmail.com>
2020-05-04 00:25:16 -0600

    rubo

--- db/migrate/20200504055745_create_dice_type.rb
+++ db/migrate/20200504055745_create_dice_type.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class CreateDiceType < ActiveRecord::Migration[5.2]
   def change
     create_table :dice_types do |t|
--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,28 +12,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_04_055745) do
-
+ActiveRecord::Schema.define(version: 20_200_504_055_745) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "dice_types", force: :cascade do |t|
-    t.string "item_type", null: false
-    t.integer "sides", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["item_type"], name: "index_dice_types_on_item_type", unique: true
+  create_table 'dice_types', force: :cascade do |t|
+    t.string 'item_type', null: false
+    t.integer 'sides', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['item_type'], name: 'index_dice_types_on_item_type', unique: true
   end
 
-  create_table "roll_items", force: :cascade do |t|
-    t.string "item_type", null: false
-    t.jsonb "content", default: "{}", null: false
-    t.integer "range_min", null: false
-    t.integer "range_max", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["content"], name: "index_roll_items_on_content", using: :gin
-    t.index ["item_type", "range_min", "range_max"], name: "index_roll_items_on_item_type_and_range_min_and_range_max"
+  create_table 'roll_items', force: :cascade do |t|
+    t.string 'item_type', null: false
+    t.jsonb 'content', default: '{}', null: false
+    t.integer 'range_min', null: false
+    t.integer 'range_max', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['content'], name: 'index_roll_items_on_content', using: :gin
+    t.index %w[item_type range_min range_max], name: 'index_roll_items_on_item_type_and_range_min_and_range_max'
   end
-
 end
